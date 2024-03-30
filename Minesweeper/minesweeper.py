ee8d0aef98aff6a9bec68a423a2940724e5f97ef
Christopher Vu <christophervu4@gmail.com>
2024-03-29 19:09:26 -0700

    Update minesweeper.py

--- Minesweeper/minesweeper.py
+++ Minesweeper/minesweeper.py
@@ -2,17 +2,25 @@ import random
 import math
 
 
-board = [[0]*16]*16
-mines = [[0]*16]*16
+board = [[0] * 16 for _ in range(16)] # CHANGE THIS
 mine_vals = random.sample(range(0, 256), 40)
 mine_locations = []
 for i in mine_vals:
     row = math.floor(i/16)
     col = (i%16) - 1
     if col==-1:
-        col = 16
+        col = 15 # change 16 to 15
     mine_locations.append((row, col))
+
+
+'''add this stuff below if u want to see the mines 
+(first line is already in carly's program)'''
 print(mine_locations)
+mines = [[0] * 16 for _ in range(16)]
+for i in mine_locations:
+    print(i[0], " ", i[1])
+    mines[i[0]][i[1]] = 1
+print(mines)
 
 
 rowInput= int(input("Which row would you like to choose (1-16)?")) - 1
