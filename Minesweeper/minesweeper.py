55d81e1f7692a8981e9f9637b869669e58bb99cd
Christopher Vu <christophervu4@gmail.com>
2024-03-29 22:30:01 -0700

    Update minesweeper.py

--- Minesweeper/minesweeper.py
+++ Minesweeper/minesweeper.py
@@ -15,16 +15,38 @@ for i in mine_vals:
 
 '''add this stuff below if u want to see the mines 
 (first line is already in carly's program)'''
-print(mine_locations)
-mines = [[0] * 16 for _ in range(16)]
-for i in mine_locations:
-    print(i[0], " ", i[1])
-    mines[i[0]][i[1]] = 1
-print(mines)
+if 1:
+    print(mine_locations)
+    mines = [[0] * 16 for _ in range(16)]
+    for i in mine_locations:
+        mines[i[0]][i[1]] = 1
+    for i in mines:
+        print(i)
 
+def count_mines(mine_locations, row, col):
+    if (row, col) in mine_locations:
+        return "mine"
 
-rowInput= int(input("Which row would you like to choose (1-16)?")) - 1
-column = int(input("Which column would you like to choose (1-16)?")) - 1
-board[rowInput][column] = 1
-for row in board:
-    print(row)
+    total_mines = 0
+    adjacent_spaces = [(row + 1, col + 1), (row + 1, col), (row + 1, col - 1),
+                       (row, col + 1), (row, col - 1),
+                       (row - 1, col + 1), (row - 1, col), (row - 1, col - 1)]
+    for location in adjacent_spaces:
+        try:
+            if location in mine_locations:
+                total_mines += 1
+        except:
+            continue
+    return total_mines
+
+def print_board(board):
+    print("".join(["_" for _ in range(32)]))
+    for row in board:
+        print(" | ".join([str(col) for col in row]))
+    print("".join(["-" for _ in range(32)]))
+
+if 1:
+    rowInput= int(input("Which row would you like to choose (1-16)?")) - 1
+    colInput = int(input("Which column would you like to choose (1-16)?")) - 1
+    print(count_mines(mine_locations, rowInput, colInput))
+    print_board(mines)
